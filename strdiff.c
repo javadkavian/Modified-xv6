8515ddd64cdf62a39465fedd8285b5f26aef1ec3
javadkavian <javadkavian8@gmail.com>
2023-10-19 16:39:22 +0330

    strdiff changed

--- strdiff.c
+++ strdiff.c
@@ -3,20 +3,15 @@
 #include "user.h"
 #include "fcntl.h"
 
-int greater_than(char a, char b){
-  int pure_ascii, pure_ascii_2;
-  if('a' <= a && a <= 'z')
-    pure_ascii = a - 'a';
-  else
-    pure_ascii = a - 'A';
-  if('a' <= b && b <= 'z')
-    pure_ascii_2 = b - 'a';
-  else
-    pure_ascii_2 = b - 'A';
 
-  if(pure_ascii >= pure_ascii_2)
-    return 0;
-  return 1;
+void make_lower_case(char* A){
+  int i = 0;
+  while(A[i] != '\0'){
+    if(!(A[i] > 'a' && A[i] < 'z')){
+      A[i] = A[i] + 32;
+    }
+    i++;
+  }
 }
 
 
@@ -27,28 +22,38 @@ main(int argc, char *argv[])
     printf(2, "Not enough arguments\n");
     exit();
   }
+  make_lower_case(argv[1]);
+  make_lower_case(argv[2]);
+  int A_size = strlen(argv[1]);
+  int B_size = strlen(argv[2]);
+  char ans[15];
+  int iterator = 0;
+  while(iterator != A_size && iterator != B_size){
+    if(argv[1][iterator] >= argv[2][iterator]){
+      ans[iterator] = '0';
+    }
+    else{
+      ans[iterator] = '1';
+    }
+    iterator++;
+  }
+  if(iterator == A_size){
+    while(iterator != B_size){
+      ans[iterator] = '1';
+      iterator ++;
+    }
+  }
+  else if(iterator == B_size){
+    while(iterator != A_size){
+      ans[iterator] = '0';
+      iterator ++;
+    }
+  }
+  ans[iterator] = '\n';
+  ans[iterator+1] = '\0';
 
-  char ans[16];
-
-  int i;
-  for(i = 0; argv[1][i] != '\0' && argv[2][i] != '\0'; i++)
-    ans[i] = '0' + greater_than(argv[1][i], argv[2][i]);
-
-  int j = i;
-  if(argv[1][i] == '\0' && argv[2][i] != '\0')
-    for(j = i; argv[2][j] != '\0'; j++)
-      ans[j] = '1';
-  else if(argv[1][i] != '\0' && argv[2][i] == '\0')
-    for(j = i; argv[1][j] != '\0'; j++)
-      ans[j] = '0';
-
-  ans[j] = '\n';
-  ans[j + 1] = '\0';
-
-  unlink("strdiff_result.txt");
   int fd = open("strdiff_result.txt", O_CREATE | O_RDWR);
   write(fd, ans, strlen(ans));
-
   close(fd);
   exit();
 }
